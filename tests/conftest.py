b8f08ecf647e0ca0444c544bea32b55e1982530e
Bryazgin Sergey <sergeybryazgin@gmail.com>
2024-01-08 14:34:30 +0300

    changes for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,36 +7,36 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "100.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    #
-    # login = os.getenv('login')
-    # password = os.getenv('password')
-    #
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    #
-    # browser.config.driver = driver
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('login')
+    password = os.getenv('password')
+
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://lime-shop.com/ru_ru/'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
     yield browser
 
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
 
@@ -44,13 +44,4 @@ def setup_browser(request):
 def open_main_page():
     browser.open('')
 
-@pytest.fixture(scope='function')
-def open_about_page():
-    browser.open('about')
 
-# @pytest.fixture(scope='function')
-# def change_to_english(setup_browser):
-#     if browser.element('.lang').should(have.text('En')):
-#         pass
-#     else:
-#         browser.element('.lang').click()
