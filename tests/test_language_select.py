455a26a1bf95c59d68a50ae33623abfa40a9c973
Bryazgin Sergey <sergeybryazgin@gmail.com>
2024-01-11 22:18:01 +0300

    edited readme, added allure tags

--- tests/test_language_select.py
+++ tests/test_language_select.py
@@ -8,7 +8,7 @@ lime_shop = LimeShop()
 @allure.tag("Diploma")
 @allure.severity(Severity.NORMAL)
 @allure.feature("Дипломный проект")
-def test_lang(open_main_page):
+def test_language_select(open_main_page):
     # WHEN
     lime_shop.click_language_selector()
     lime_shop.select_kazakhstan_language()
